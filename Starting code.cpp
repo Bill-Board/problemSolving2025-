d6da25bf81284c1a0e2f24234cb57df12186cff5
Shoeb Akibul Islam <59358725+Bill-Board@users.noreply.github.com>
2025-05-31 21:28:13 +0600

    Initial commit

---
+++ Starting code.cpp
@@ -0,0 +1,35 @@
+//BISMILLAHIR RAHMANIR RAHEEM
+//ALLAH IS WATCHING ME
+// Shoeb Akibul Islam
+// Dept of ICE, NSTU
+#include<bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using pii= pair<int,int>;
+
+const int N = 100005;
+ll mod= 1e9+7;
+
+void solve(int tc)
+{
+    int n; cin >> n;
+
+    cout << n << endl;
+}
+
+
+
+signed main()
+{
+    int test_case=1;
+//    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+//    cin >> test_case;
+    for(int i = 1; i <= test_case ; i++)
+        solve(i);
+    return 0;
+}
+
+
+
+///  Alhamdulillah...
